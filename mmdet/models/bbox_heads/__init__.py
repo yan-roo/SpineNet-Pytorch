87d81567f6b6df79919ca2d771a9cdeefa23c39e
yan-roo <49684416+yan-roo@users.noreply.github.com>
2020-10-16 14:01:42 +0800

    Undo modification of bbox_heads/convfc_bbox_head
    Remove Shared4Conv1FCBBoxHeaed

--- mmdet/models/bbox_heads/__init__.py
+++
@@ -1,7 +0,0 @@
-from .bbox_head import BBoxHead
-from .convfc_bbox_head import ConvFCBBoxHead, SharedFCBBoxHead, Shared4Conv1FCBBoxHead
-from .double_bbox_head import DoubleConvFCBBoxHead
-
-__all__ = [
-    'BBoxHead', 'ConvFCBBoxHead', 'SharedFCBBoxHead', 'DoubleConvFCBBoxHead', 'Shared4Conv1FCBBoxHead'
-]
